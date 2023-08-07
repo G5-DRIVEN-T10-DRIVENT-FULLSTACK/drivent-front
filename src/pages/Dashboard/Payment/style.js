94f2e4fc7a694118123632d13f90cb6ef03fb0b1
FernandoM52 <fernandomartins.contato@outlook.com>
2023-08-07 12:24:44 -0300

    fix/ PaymentPage - Style da mensagem de aviso quando usuário não possui inscrição ao visualizar a tela

--- src/pages/Dashboard/Payment/style.js
+++ src/pages/Dashboard/Payment/style.js
@@ -169,14 +169,12 @@ export const WarningMessageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  text-align: center;
   width: 100%;
   height: 100%;
-
-  p{
-    font-family: 'Roboto', sans-serif;
-    color: #8E8E8E;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 23.44px;
-  }
+  font-family: 'Roboto', sans-serif;
+  color: #8E8E8E;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23.44px;
 `;
